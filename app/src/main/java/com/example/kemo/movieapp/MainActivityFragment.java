682449da173b6bda76c6d0c4e0e1ee2a999763ab
kareem <karim.salah2048@gmail.com>
2016-10-18 10:30:59 +0200

    update image adabter
    image adapter now works with MDB and loading images correctly

--- app/src/main/java/com/example/kemo/movieapp/MainActivityFragment.java
+++ app/src/main/java/com/example/kemo/movieapp/MainActivityFragment.java
@@ -1,91 +1,237 @@
 package com.example.kemo.movieapp;
 
 import android.content.Context;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.GridView;
-import android.widget.ImageView;
+import android.widget.*;
 import com.squareup.picasso.Picasso;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
 
 /**
  * A placeholder fragment containing a simple view.
  */
+//TODO
+    //a class contains data & may Inherit it from image view
 public class MainActivityFragment extends Fragment {
 
-    public static String[] eatFoodyImages = {
-            "http://i.imgur.com/rFLNqWI.jpg",
-            "http://i.imgur.com/C9pBVt7.jpg",
-            "http://i.imgur.com/rT5vXE1.jpg",
-            "http://i.imgur.com/aIy5R2k.jpg",
-            "http://i.imgur.com/MoJs9pT.jpg",
-            "http://i.imgur.com/S963yEM.jpg",
-            "http://i.imgur.com/rLR2cyc.jpg",
-            "http://i.imgur.com/SEPdUIx.jpg",
-            "http://i.imgur.com/aC9OjaM.jpg",
-            "http://i.imgur.com/76Jfv9b.jpg",
-            "http://i.imgur.com/fUX7EIB.jpg",
-            "http://i.imgur.com/syELajx.jpg",
-            "http://i.imgur.com/COzBnru.jpg",
-            "http://i.imgur.com/Z3QjilA.jpg",
-    };
+
     public MainActivityFragment() {
     }
-//Still didnt work on it
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        fetchMovies();
+    }
+
+    public ImageListAdapter imageListAdapter;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
        GridView gridView =(GridView) view.findViewById(R.id.gridView_movies);
-        ImageListAdapter imageListAdapter = new ImageListAdapter(getActivity(), eatFoodyImages);
+         imageListAdapter = new ImageListAdapter(getActivity(), new String[0]);
         gridView.setAdapter(imageListAdapter);
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String movie =(String) imageListAdapter.getItem(i);
+                Toast.makeText(getActivity(), movie, Toast.LENGTH_SHORT).show();
+            }
+        });
         return view;
     }
     private void fetchMovies()
     {
-
+        FetchMovies fetchMovies = new FetchMovies();
+        fetchMovies.execute("popularity.desc");
     }
-    class FetchMovies extends AsyncTask<String, Void, String[]>
+    class FetchMovies extends AsyncTask<String, Void,ArrayList<Movie>>
     {
         @Override
-        protected String[] doInBackground(String... strings) {
-            String name = "asd";
-            return new String[0];
+        protected ArrayList<Movie> doInBackground(String... sortMode)   {
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+            String moviesJson = "";
+            try
+            {
+                URL url = fetchURL(sortMode[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                moviesJson = buffer.toString();
+            }catch (Exception e)
+            {
+                Log.e("exeption", e.getMessage());
+            }finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e("PlaceholderFragment", "Error closing stream", e);
+                    }
+                }
+            }
+            try{
+                return getMovieFromjson(moviesJson);
+            }catch (Exception e)
+            {
+                Log.w("Tag", e.getMessage());
+                return null;
+            }
+
+        }
+        private URL fetchURL(String sortMode) throws MalformedURLException {
+            /*String baseURL = "https://api.themoviedb.org/3/discover/movie?";
+            String sortby = true ? "sort_by=popularity.desc" :  "sort_by=vote_average.desc";
+            return baseURL + sortby + BuildConfig.THE_MOVIE_DATABASE_KEY;*/
+            final String MOVIES_BASE_URL =
+                    "https://api.themoviedb.org/3/discover/movie?";
+
+            final String SORTBY = "sort_by";
+            final String key = "api_key";
+
+            Uri builtUri = Uri.parse(MOVIES_BASE_URL).buildUpon()
+                    .appendQueryParameter(SORTBY, sortMode)
+                    .appendQueryParameter(key, BuildConfig.THE_MOVIE_DATABASE_KEY)
+                    .build();
+            Log.w("Tag", builtUri.toString());
+            return new URL(builtUri.toString());
+        }
+        private  ArrayList<Movie> getMovieFromjson(String jsonStr) throws JSONException {
+            final String MDB_RESULTS = "results";
+            final String MDB_ID = "id";
+            final String MDB_POSTER_PATH = "poster_path";
+            final String MDB_OVERVIEW = "overview";
+            final String MDB_TITLE = "title";
+            final String MDB_RELEASE_DATE = "release_date";
+            final String MDB_VOTE_AVERAGE = "vote_average";
+            final String MDB_POPULARITY = "popularity";
+            final String MDB_ORIGINAL_TITLE = "original_title";
+            ArrayList<Movie> movies = new ArrayList<>();
+            JSONObject movieDetails = new JSONObject(jsonStr);
+            JSONArray weatherData = movieDetails.getJSONArray(MDB_RESULTS);
+            for (int i = 0; i < weatherData.length(); i++)
+            {
+                Movie movie = new Movie(getActivity());
+                JSONObject jsonObject = weatherData.getJSONObject(i);
+                movie.setMovieId(jsonObject.getInt(MDB_ID));
+                movie.setPosterPath(jsonObject.getString(MDB_POSTER_PATH));
+                movie.setOverView(jsonObject.getString(MDB_OVERVIEW));
+                movie.setTitle(jsonObject.getString(MDB_TITLE));
+                movie.setReleaseData(jsonObject.getString(MDB_RELEASE_DATE));
+                movie.setVoteAverage(jsonObject.getDouble(MDB_VOTE_AVERAGE));
+                movie.setPopularity(jsonObject.getDouble(MDB_POPULARITY));
+                movie.setOriginalTitle(jsonObject.getString(MDB_ORIGINAL_TITLE));
+                movies.add(movie);
+            }
+
+            return movies;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<Movie> movies) {
+            super.onPostExecute(movies);
+//            imageListAdapter.clear();
+            ArrayList<String> uriPaths = imageListAdapter.getUriList();
+            uriPaths.clear();
+            for (Movie movie: movies
+                 ) {
+                Log.w("tag", movie.getPosterPath());
+                uriPaths.add(movie.getPosterPath());
+                //imageListAdapter.add(movie.getPosterPath());
+            }
+            imageListAdapter.notifyDataSetChanged();
         }
     }
 
 
 }
-class ImageListAdapter extends ArrayAdapter {
+class ImageListAdapter extends BaseAdapter {
     private Context context;
     private LayoutInflater inflater;
-
-    private String[] imageUrls;
+    private ArrayList<String> imageUrls;
 
     public ImageListAdapter(Context context, String[] imageUrls) {
-        super(context, R.layout.movies_posters, imageUrls);
-
+        this.imageUrls = new ArrayList<>();
         this.context = context;
-        this.imageUrls = imageUrls;
-
+        this.imageUrls.toArray(imageUrls);
         inflater = LayoutInflater.from(context);
     }
 
     @Override
+    public Object getItem(int i) {
+        return imageUrls.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public int getCount() {
+        return imageUrls.size();
+    }
+
+    @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        if (null == convertView) {
-            convertView = inflater.inflate(R.layout.movies_posters, parent, false);
-        }
+        ImageView imageView;
+           // convertView = inflater.inflate(R.layout.movies_posters, parent, false);
+
+            if (convertView == null) {
+                // if it's not recycled, initialize some attributes
+                imageView = new ImageView(context);
+                //imageView.setLayoutParams(new GridView.LayoutParams(300, 300));
+                imageView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 300));
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            } else {
+                imageView = (ImageView) convertView;
+            }
+
+
         Picasso
                 .with(context)
-                .load(imageUrls[position])
+                .load(imageUrls.get(position))
                 .fit()
-                .into((ImageView) convertView);
+                .into( imageView);
 
-        return convertView;
+        return imageView;
+    }
+    public ArrayList<String> getUriList()
+    {
+        return imageUrls;
     }
 }
---
+++ app/src/main/java/com/example/kemo/movieapp/Movie.java
@@ -0,0 +1,90 @@
+package com.example.kemo.movieapp;
+
+import android.content.Context;
+import android.widget.ImageView;
+
+/**
+ * Created by kemo on 17/10/2016.
+ */
+public class Movie extends ImageView{
+    private String title;
+    private String overView;
+    private double voteAverage;
+    private int movieId;
+    private String releaseData;
+    private String posterPath;
+    private double popularity;
+    private String originalTitle;
+    public String getOriginalTitle() {
+        return originalTitle;
+    }
+
+    public void setOriginalTitle(String originalTitle) {
+        this.originalTitle = originalTitle;
+    }
+    public double getPopularity() {
+        return popularity;
+    }
+
+    public void setPopularity(double popularity) {
+        this.popularity = popularity;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = "http://image.tmdb.org/t/p/w185/" + posterPath;
+    }
+
+
+    public double getVoteAverage() {
+        return voteAverage;
+    }
+
+    public void setVoteAverage(double voteAverage) {
+        this.voteAverage = voteAverage;
+    }
+
+    public int getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(int movieId) {
+        this.movieId = movieId;
+    }
+
+    public String getReleaseData() {
+        return releaseData;
+    }
+
+    public void setReleaseData(String releaseData) {
+        this.releaseData = releaseData;
+    }
+
+
+
+    public String getOverView() {
+        return overView;
+    }
+
+    public void setOverView(String overview) {
+        this.overView = overview;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+
+    public Movie(Context context)
+    {
+        super(context);
+
+    }
+}
