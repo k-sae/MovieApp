71814400fb71f2398ad6cc7dd1079af4218a16e4
kareem <karim.salah2048@gmail.com>
2016-11-22 01:47:05 +0200

    fix issue with tablet ui

--- app/src/main/java/com/example/kemo/movieapp/DetailsActivityFragment.java
+++ app/src/main/java/com/example/kemo/movieapp/DetailsActivityFragment.java
@@ -1,6 +1,5 @@
 package com.example.kemo.movieapp;
 
-import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -8,11 +7,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.*;
-import com.squareup.picasso.Picasso;
-import io.realm.Realm;
-import io.realm.RealmResults;
-import org.apmem.tools.layouts.FlowLayout;
+import android.widget.ListView;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -30,10 +25,9 @@ public class DetailsActivityFragment extends Fragment {
         DetailsActivityFragment.movie = movie;
     }
     DetailsAdapter detailsAdapter;
-    private boolean isInFavourites;
     public DetailsActivityFragment() {
     }
-    FlowLayout trailersViewer;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -41,7 +35,7 @@ public class DetailsActivityFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_details, container, false);
         //get the movie View
         View movieDetailsView = inflater.inflate(R.layout.movie_details, container, false);
-        movieDetailsView = setLayout(movieDetailsView,container);
+//        movieDetailsView = setLayout(movieDetailsView,container);
         //initialize arrayList
         ArrayList<Object> objects = new ArrayList<>();
         //add movie to this arrayList
@@ -55,157 +49,7 @@ public class DetailsActivityFragment extends Fragment {
         Log.e("ID","" + movie.getMovieId());
         return view;
     }
-    private View setLayout(View view, ViewGroup container)
-    {
-        //loading Image
-        ImageView imageView = (ImageView) view.findViewById(R.id.Detail_poster_imageView);
-        Button favouritesButton =(Button) view.findViewById(R.id.favourites_button);
-        setfavouriteButton(favouritesButton);
-         Picasso.with(getActivity()).load(movie.getPosterPath()).fit().into(imageView);
-        //title
-        TextView textView = (TextView) view.findViewById(R.id.movieTitle_textView);
-        textView.setText(textView.getText() +" " + movie.getTitle());
-        //date
-        textView = (TextView) view.findViewById(R.id.movieDate_textView);
-        AppendText(textView, movie.getReleaseData());
-        //Description
-        textView = (TextView) view.findViewById(R.id.movieDescription_textView);
-        AppendText(textView, "\n" +movie.getOverView());
-        //rating
-        textView = (TextView) view.findViewById(R.id.movieVoteAverage_textView);
-        AppendText(textView,Double.toString(movie.getVoteAverage()));
-        //popularity
-        textView = (TextView) view.findViewById(R.id.moviePopularity);
-        AppendText(textView,Double.toString(movie.getPopularity()));
-        trailersViewer = (FlowLayout) view.findViewById(R.id.trailers_container);
-        fetchTrailers(movie.getMovieId());
-        return view;
-    }
-    private void AppendText(TextView textView, String text)
-    {
-        textView.setText(textView.getText() +" " + text);
-    }
-    private void fetchTrailers(int id)
-    {
-        URLConnector urlConnector = new URLConnector() {
-            @Override
-            protected ArrayList<Object> getObjFromjson(String jsonStr) {
-                ArrayList<Object> movieTrailers = new ArrayList<>();
-                final String NAME = "name";
-                final String KEY = "key";
-                final String MDB_RESULTS = "results";
-                try
-                {
-                    JSONObject movieTrailersDetails = new JSONObject(jsonStr);
 
-                    JSONArray movieTrailerData = movieTrailersDetails.getJSONArray(MDB_RESULTS);
-                    for (int i = 0; i< movieTrailerData.length(); i++)
-                    {
-                        JSONObject jsonObject = movieTrailerData.getJSONObject(i);
-                        MovieTrailer movieTrailer = new MovieTrailer();
-                        movieTrailer.setKey(jsonObject.getString(KEY));
-                        movieTrailer.setName(jsonObject.getString(NAME));
-                        movieTrailers.add(movieTrailer);
-                    }
-                    return movieTrailers;
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-                return null;
-            }
-
-            @Override
-            protected URL fetchURL(String s) {
-                final String BASE_URL =  "https://api.themoviedb.org/3/movie/" + s + "/videos?";
-                final String KEY = "api_key";
-                Uri builtUri = Uri.parse(BASE_URL).buildUpon().
-                        appendQueryParameter(KEY,BuildConfig.THE_MOVIE_DATABASE_KEY).build();
-                try {
-                    return new URL(builtUri.toString());
-                } catch (MalformedURLException e) {
-                    Log.e("FetchTrailers", e.getMessage());
-                }
-                return null;
-            }
-
-            @Override
-            protected void onPostExecute(ArrayList<Object> objects) {
-                super.onPostExecute(objects);
-                if (objects == null) return;
-                trailersViewer.findViewById(R.id.movie_trailers_loading).setVisibility(View.GONE);
-                for (Object o :
-                        objects) {
-                    try {
-                        final MovieTrailer movieTrailer = (MovieTrailer) o;
-                        LinearLayout linearLayout = (LinearLayout) LayoutInflater.from(getActivity())
-                                .inflate(R.layout.movie_trailer, null, false);
-                        TextView textView = (TextView) linearLayout.findViewById(R.id.trailer_textView);
-                        textView.setText(movieTrailer.getName());
-                        linearLayout.setOnClickListener(new View.OnClickListener() {
-                            @Override
-                            public void onClick(View view) {
-                                startActivity
-                                        (new Intent(Intent.ACTION_VIEW,
-                                                Uri.parse("http://www.youtube.com/watch?v="
-                                                        + movieTrailer.getKey())));
-                            }
-                        });
-                        trailersViewer.addView(linearLayout);
-                    }catch (Exception e)
-                    {
-                        try {
-                            Log.e("DetailFragment", e.getMessage());
-                        }catch (Exception e1)
-                        {
-                            //ignore
-                        }
-                    }
-                }
-            }
-        };
-
-        urlConnector.execute(Integer.toString(id));
-    }
-   private boolean isInFavourites()
-   {
-       //SetRealm
-       Realm realm = Realm.getDefaultInstance();
-
-        RealmResults<Movie> realmQuery = realm.where(Movie.class).equalTo("movieId", movie.getMovieId()).findAll();
-       return (realmQuery.size() != 0);
-
-   }
-
-   private void setfavouriteButton(final Button favouritesButton)
-   {
-         isInFavourites = isInFavourites();
-       updateLayout(isInFavourites, favouritesButton);
-       favouritesButton.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View view) {
-                if (isInFavourites)
-                {
-                    Utility.removeFromRealm(movie.getMovieId());
-                }
-                else Utility.addToRealm(movie);
-               isInFavourites = !isInFavourites;
-               updateLayout(isInFavourites, favouritesButton);
-           }
-       });
-   }
-
-   private void updateLayout(boolean b, Button favButton)
-   {
-       if(b)
-       {
-
-           favButton.setText(getString(R.string.remove_from_favourites_button));
-       }
-       else
-       {
-           favButton.setText(getString(R.string.add_to_favourites_button));
-       }
-   }
     private void fetchReviews(int id)
     {
         URLConnector urlConnector = new URLConnector() {
--- app/src/main/java/com/example/kemo/movieapp/DetailsAdapter.java
+++ app/src/main/java/com/example/kemo/movieapp/DetailsAdapter.java
@@ -1,14 +1,23 @@
 package com.example.kemo.movieapp;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
+import android.widget.*;
+import com.squareup.picasso.Picasso;
+import io.realm.Realm;
+import io.realm.RealmResults;
+import org.apmem.tools.layouts.FlowLayout;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 
 /**
@@ -18,6 +27,8 @@ public class DetailsAdapter extends BaseAdapter {
     private Context context;
     private ArrayList<Object> items;
     private View movieView;
+    private boolean isInFavourites;
+    FlowLayout trailersViewer;
     public DetailsAdapter(Context context, ArrayList<Object> items, View movieView)
     {
         this.items = items;
@@ -42,7 +53,6 @@ public class DetailsAdapter extends BaseAdapter {
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
         Object item = items.get(i);
-
         if(item instanceof Review) {
             if (view == null || !(view instanceof LinearLayout))
                view = LayoutInflater.from(context).inflate(R.layout.movie_review, viewGroup, false);
@@ -50,8 +60,10 @@ public class DetailsAdapter extends BaseAdapter {
             ((TextView)view.findViewById(R.id.review_content_textView)).setText("\""+((Review)item).getContent()+ "\"");
         }
         else if (item instanceof Movie) {
-            if (view == null || !(view instanceof RelativeLayout))
-            view = movieView;
+            if (view == null || !(view instanceof RelativeLayout)) {
+                view = LayoutInflater.from(context).inflate(R.layout.movie_details, viewGroup, false);
+                view = setLayout(view, viewGroup, (Movie) item);
+            }
         }
 
         return view;
@@ -59,5 +71,161 @@ public class DetailsAdapter extends BaseAdapter {
     public ArrayList<Object> getItems()
     {
         return items;
+    }
+
+
+
+
+
+    private View setLayout(View view, ViewGroup container, Movie movie)
+    {
+        //loading Image
+        ImageView imageView = (ImageView) view.findViewById(R.id.Detail_poster_imageView);
+        Button favouritesButton =(Button) view.findViewById(R.id.favourites_button);
+        setfavouriteButton(favouritesButton);
+        Picasso.with(context).load(movie.getPosterPath()).fit().into(imageView);
+        //title
+        TextView textView = (TextView) view.findViewById(R.id.movieTitle_textView);
+        textView.setText(textView.getText() +" " + movie.getTitle());
+        //date
+        textView = (TextView) view.findViewById(R.id.movieDate_textView);
+        AppendText(textView, movie.getReleaseData());
+        //Description
+        textView = (TextView) view.findViewById(R.id.movieDescription_textView);
+        AppendText(textView, "\n" +movie.getOverView());
+        //rating
+        textView = (TextView) view.findViewById(R.id.movieVoteAverage_textView);
+        AppendText(textView,Double.toString(movie.getVoteAverage()));
+        //popularity
+        textView = (TextView) view.findViewById(R.id.moviePopularity);
+        AppendText(textView,Double.toString(movie.getPopularity()));
+        trailersViewer = (FlowLayout) view.findViewById(R.id.trailers_container);
+        fetchTrailers(movie.getMovieId());
+        return view;
+    }
+    private void AppendText(TextView textView, String text)
+    {
+        textView.setText(textView.getText() +" " + text);
+    }
+    private void fetchTrailers(int id)
+    {
+        URLConnector urlConnector = new URLConnector() {
+            @Override
+            protected ArrayList<Object> getObjFromjson(String jsonStr) {
+                ArrayList<Object> movieTrailers = new ArrayList<>();
+                final String NAME = "name";
+                final String KEY = "key";
+                final String MDB_RESULTS = "results";
+                try
+                {
+                    JSONObject movieTrailersDetails = new JSONObject(jsonStr);
+
+                    JSONArray movieTrailerData = movieTrailersDetails.getJSONArray(MDB_RESULTS);
+                    for (int i = 0; i< movieTrailerData.length(); i++)
+                    {
+                        JSONObject jsonObject = movieTrailerData.getJSONObject(i);
+                        MovieTrailer movieTrailer = new MovieTrailer();
+                        movieTrailer.setKey(jsonObject.getString(KEY));
+                        movieTrailer.setName(jsonObject.getString(NAME));
+                        movieTrailers.add(movieTrailer);
+                    }
+                    return movieTrailers;
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+            @Override
+            protected URL fetchURL(String s) {
+                final String BASE_URL =  "https://api.themoviedb.org/3/movie/" + s + "/videos?";
+                final String KEY = "api_key";
+                Uri builtUri = Uri.parse(BASE_URL).buildUpon().
+                        appendQueryParameter(KEY,BuildConfig.THE_MOVIE_DATABASE_KEY).build();
+                try {
+                    return new URL(builtUri.toString());
+                } catch (MalformedURLException e) {
+                    Log.e("FetchTrailers", e.getMessage());
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(ArrayList<Object> objects) {
+                super.onPostExecute(objects);
+                if (objects == null) return;
+                trailersViewer.findViewById(R.id.movie_trailers_loading).setVisibility(View.GONE);
+                for (Object o :
+                        objects) {
+                    try {
+                        final MovieTrailer movieTrailer = (MovieTrailer) o;
+                        LinearLayout linearLayout = (LinearLayout) LayoutInflater.from(context)
+                                .inflate(R.layout.movie_trailer, null, false);
+                        TextView textView = (TextView) linearLayout.findViewById(R.id.trailer_textView);
+                        textView.setText(movieTrailer.getName());
+                        linearLayout.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                               context.startActivity
+                                        (new Intent(Intent.ACTION_VIEW,
+                                                Uri.parse("http://www.youtube.com/watch?v="
+                                                        + movieTrailer.getKey())));
+                            }
+                        });
+                        trailersViewer.addView(linearLayout);
+                    }catch (Exception e)
+                    {
+                        try {
+                            Log.e("DetailFragment", e.getMessage());
+                        }catch (Exception e1)
+                        {
+                            //ignore
+                        }
+                    }
+                }
+            }
+        };
+
+        urlConnector.execute(Integer.toString(id));
+    }
+    private boolean isInFavourites()
+    {
+        //SetRealm
+        Realm realm = Realm.getDefaultInstance();
+
+        RealmResults<Movie> realmQuery = realm.where(Movie.class).equalTo("movieId", ((Movie)items.get(0)).getMovieId()).findAll();
+        return (realmQuery.size() != 0);
+
+    }
+
+    private void setfavouriteButton(final Button favouritesButton)
+    {
+        isInFavourites = isInFavourites();
+        updateLayout(isInFavourites, favouritesButton);
+        favouritesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isInFavourites)
+                {
+                    Utility.removeFromRealm(((Movie)items.get(0)).getMovieId());
+                }
+                else Utility.addToRealm(((Movie)items.get(0)));
+                isInFavourites = !isInFavourites;
+                updateLayout(isInFavourites, favouritesButton);
+            }
+        });
+    }
+
+    private void updateLayout(boolean b, Button favButton)
+    {
+        if(b)
+        {
+
+            favButton.setText(context.getString(R.string.remove_from_favourites_button));
+        }
+        else
+        {
+            favButton.setText(context.getString(R.string.add_to_favourites_button));
+        }
     }
 }
